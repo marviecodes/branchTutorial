d81fac313278ed817bce2c3bbea9129bc78150d8
Marvelous Osondu <marvnonso554@gmail.com>
2024-06-18 22:24:49 +0100

    Add your new feature changes

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
 	public static void main(String args[]) {
 		System.out.println("Hello, there!");
 		System.out.println("Marvelous' feature was added here.");
+		System.out.println("Your new feature will be added here.");
 	}
 }
